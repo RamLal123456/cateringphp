154b2d6563392f6787ba8f1cb64b322e8575dd84
Ramlal <ramugupta1520@gmail.com>
2024-09-17 13:47:07 +0530

    Initial commit

---
+++ user.php
@@ -0,0 +1,125 @@
+<?php
+include 'include/header.php';
+?>
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+  <ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+      <a class="nav-link " href="">
+        <img src="images/about.jpg" style="width:50px; border-radius:50px" alt="">
+        <span style="font-size:20px">Hiii Ram</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link " href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:13px">MY ORDERS</span>
+      </a>
+    </li><!-- End Dashboard Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-person-fill"></i>
+        <span style="font-size:20px">ACCOUNT SETTING</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">Profile Information</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">Manage Addresses</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">PAN Card Information</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-credit-card"></i>
+        <span style="font-size:20px">PAYMENTS</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">Gift Cards</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">Saved UPI</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">Saved Cards</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:20px">MY STUFF</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">My Coupons</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">My Reviews & Ratings</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">All Notification</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="">
+        <i class="bi bi-"></i>
+        <span style="font-size:15px">My Wishlist</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="admin/logout.php">
+        <i class="bi bi-glyphicon-off"></i>
+        <span style="font-size:18px">LOG OUT</span>
+      </a>
+    </li>
+  </ul>
+
+</aside><!-- End Sidebar-->
+<?php
+include 'include/footer.php';
+?>
